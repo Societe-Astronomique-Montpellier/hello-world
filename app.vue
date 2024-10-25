93abdbd2cad52958bb0b7c0f2802c21d72faec2a
smeaudre.ext <smeaudre.ext@free-mobile.fr>
2024-10-25 14:31:54 +0200

    Add env + test template

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+const title: Ref<string> = ref('Hellow world');
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="hello">
+    <p>{{ title }}</p>
+    <p>{{ runtimeConfig.public.test_env }}</p>
   </div>
 </template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
+  runtimeConfig: {
+    public: {
+      test_env: process.env.NUXT_TEST_ENV
+    }
+  }
 })
